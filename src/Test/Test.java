7a9607488b68a82194fd4db384957d8100707915
lyubick <andrejs.lubimovs@gmail.com>
2015-02-01 15:53:02 +0200

    Tests.

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -5,24 +5,62 @@ package Test;
 
 import Logger.Logger;
 import Logger.LOGLEVELS;
+
 import RSA.RSA;
+
+import Common.ReturnCodes;
 
 /**
  * @author lyubick
  *
  */
-public class Test {
+public class Test
+{
+    private static int TestNr = 0;
+
+    public static void launchTest(ReturnCodes result)
+    {
+        ++TestNr;
+
+        if (result.equals(ReturnCodes.RC_OK))
+        {
+            System.out.println(TestNr + ": FAILED!");
+        }
+        else
+        {
+            System.out.println(TestNr + ": PASSED!");
+        }
+    }
+
+/* Test set */
+    public static ReturnCodes TestRSA()
+    {
+        RSA rsa = new RSA("12345", "54321", "6789");
+
+        if (!rsa.getAuthorizationStatus().equals("PASS))"))
+                return ReturnCodes.RC_NOK;
+
+        rsa = new RSA("97531", "13579", "5463");
+
+        if (!rsa.getAuthorizationStatus().equals("PASS))"))
+                return ReturnCodes.RC_NOK;
+
+        rsa = new RSA("15156", "6855", "232232");
+
+        if (!rsa.getAuthorizationStatus().equals("PASS))"))
+                return ReturnCodes.RC_NOK;
+
+        return ReturnCodes.RC_OK;
+    }
 
     /**
      * @param args
      */
     public static void main(String[] args)
     {
-        Logger.loggerON(LOGLEVELS.DEBUG);
+        Logger.loggerON(LOGLEVELS.SILENT);
 
-        RSA rsa = new RSA("12345", "54321", "6789");
-
-        System.out.println(rsa.getAuthorizationStatus());
+        /*1. */ launchTest(TestRSA());
 
         Logger.loggerOFF();
     }
