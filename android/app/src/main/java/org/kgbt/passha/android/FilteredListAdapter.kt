884480a1fe7e95a1c8552467daefe91336f5f3fa
curious_odd_man <vlad.varslavans@gmail.com>
2018-04-24 22:46:31 +0300

    BROKEN.
    Added Android project. Still doesn't work, but looks close.

---
+++ android/app/src/main/java/org/kgbt/passha/android/FilteredListAdapter.kt
@@ -0,0 +1,85 @@
+package org.kgbt.passha.android
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import org.kgbt.passha.core.db.SpecialPassword
+import org.kgbt.passhamobile.R
+
+class FilteredListAdapter(context: Context?, resource: Int, objects: List<SpecialPassword>?) : BaseAdapter(), Filterable {
+
+    val TAG = "FilteredListAdapter"
+
+    val aSpecialPasswords = objects!!
+    var aFilteredPasswords = objects!!
+    val aInflater = LayoutInflater.from(context);
+    val aResource = resource
+
+    // Example is here: https://www.simplifiedcoding.net/custom-listview-android/
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        //getting the view
+        val view = aInflater.inflate(aResource, null, false)
+
+        //getting the view elements of the list from the view
+        val textViewName = view.findViewById<TextView>(R.id.textView)
+
+        //adding values to the list item
+        textViewName.setText(aFilteredPasswords[position].getName())
+
+        return view
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val results = FilterResults()        // Holds the results of a filtering operation in values
+                val filteredList = ArrayList<SpecialPassword>()
+
+                if (constraint == null || constraint.isEmpty() || constraint.isBlank()) {
+
+                    // set the Original result to return
+                    results.count = aSpecialPasswords.size
+                    results.values = aSpecialPasswords
+                } else {
+                    val lowerConstraint: String = constraint.toString().toLowerCase()
+                    for (i in 0 until aSpecialPasswords.size) {
+                        val data = aSpecialPasswords.get(i)
+                        if (data.name.toLowerCase().contains(lowerConstraint)) {
+                            filteredList.add(data)
+                        }
+                    }
+                    // set the Filtered result to return
+                    results.count = filteredList.size
+                    results.values = filteredList
+                }
+
+                val lst = (results.values as ArrayList<SpecialPassword>).map { sp -> sp.name }
+
+                Log.d(TAG, lst.toString())
+
+                return results
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                aFilteredPasswords = results!!.values as List<SpecialPassword> // has the filtered values
+                notifyDataSetChanged()  // notifies the data with new filtered values
+            }
+        }
+    }
+
+    override fun getItem(position: Int): Any {
+        return aFilteredPasswords[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return aFilteredPasswords[position].name.hashCode().toLong()
+    }
+
+    override fun getCount(): Int {
+        return aFilteredPasswords.size
+    }
+
+}
---
+++ android/app/src/main/java/org/kgbt/passha/android/LoginActivity.kt
@@ -0,0 +1,125 @@
+package org.kgbt.passha.android
+
+import android.Manifest
+import android.support.v7.app.AppCompatActivity
+import android.app.LoaderManager.LoaderCallbacks
+
+import android.database.Cursor
+import android.os.AsyncTask
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+
+import android.content.*
+import android.util.Log
+import android.widget.*
+import android.support.v4.content.ContextCompat
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Environment
+import android.content.Intent
+import org.kgbt.passha.core.VaultManager
+import org.kgbt.passha.core.common.Exceptions
+import org.kgbt.passha.core.logger.Logger
+import org.kgbt.passha.core.logger.Output
+import org.kgbt.passhamobile.R
+
+
+class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
+
+    private val TAG = "LoginView"
+
+    // UI references.
+    private var aPasswordView: EditText? = null
+    private var aLoginFormView: View? = null
+
+
+    // HERE STARTS COPYPASTED CODE: https://stackoverflow.com/questions/20714058/file-exists-and-is-directory-but-listfiles-returns-null
+    val EXTERNAL_PERMS = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    val EXTERNAL_REQUEST = 138          // This is copypasted along with request permission code. Dunno why its 138
+
+    private fun requestForPermission(): Boolean {
+        var isPermissionOn = true
+        val version = Build.VERSION.SDK_INT
+        if (version >= 23) {
+            if (!canAccessExternalSd()) {
+                isPermissionOn = false
+                requestPermissions(EXTERNAL_PERMS, EXTERNAL_REQUEST)
+            }
+        }
+
+        return isPermissionOn
+    }
+
+    private fun canAccessExternalSd(): Boolean {
+        return hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+
+    private fun hasPermission(perm: String): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, perm)
+    }
+
+    // HERE ENDS COPYPASTED CODE
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        requestForPermission()
+        // Set up the login form.
+        aPasswordView = findViewById(R.id.password)
+        aLoginFormView = findViewById(R.id.login_form)
+
+        val goButton = findViewById<Button>(R.id.locate_file_button)
+        goButton.setOnClickListener { attemptLogin() }
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private fun attemptLogin() {
+        // Reset errors.
+        aPasswordView!!.error = null
+
+        // Store values at the time of the login attempt.
+        val password = aPasswordView!!.text.toString()
+
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            Log.e(TAG, "Password is empty!");
+            aPasswordView!!.error = getString(R.string.error_field_required)
+            aPasswordView!!.requestFocus()
+        } else {
+            Log.v(TAG, "Try authentication");
+            try {
+                Logger.loggerON(null, PasshaLogsOutput())
+                VaultManager.init()
+                val vault = VaultManager.getInstance().addVault(password, false)
+                val intent = Intent(baseContext, VaultActivity::class.java);
+                intent.putExtra("VAULT_NAME", vault.name)
+                startActivity(intent)
+            } catch (e: Exceptions) {
+                Log.e(TAG, "Error", e)
+                aPasswordView!!.error = getString(R.string.error_incorrect_password)
+                aPasswordView!!.requestFocus()
+            }
+        }
+    }
+
+    override fun onCreateLoader(i: Int, bundle: Bundle): Loader<Cursor> {
+        // Not sure if this is required, or can just pass null
+        return CursorLoader(this)
+    }
+
+    override fun onLoadFinished(cursorLoader: Loader<Cursor>, cursor: Cursor) {
+    }
+
+    override fun onLoaderReset(cursorLoader: Loader<Cursor>) {
+
+    }
+}
+
