91e0da23eac5d4a2a8ff704e424089cc850812ab
lyubick <andrejs.lubimovs@gmail.com>
2020-11-29 17:57:05 +0200

    RELEASE.
    v3.1.000

--- core/src/main/java/org/kgbt/passha/core/common/cfg/Properties.java
+++ core/src/main/java/org/kgbt/passha/core/common/cfg/Properties.java
@@ -5,7 +5,7 @@ public final class Properties
     public final class SOFTWARE
     {
         public static final String NAME    = "pasSHA";
-        public static final String VERSION = "3.0";
+        public static final String VERSION = "3.1";
     }
 
     public final class PATHS
